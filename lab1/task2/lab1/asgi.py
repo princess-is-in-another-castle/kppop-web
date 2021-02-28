ae714fa768d06b8613fff561a1d382fd02ba7752
princess-is-in-another-castle <rus31344@gmail.com>
2021-03-01 00:30:42 +0300

    fix django project, now everything works

--- lab1/task2/lab1/asgi.py
+++ lab1/task2/lab1/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for lab project.
+ASGI config for lab1 project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lab.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lab1.settings')
 
 application = get_asgi_application()
