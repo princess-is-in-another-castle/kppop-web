8054b2b3471391e5c278d36e04c07b1bb9102269
princess-is-in-another-castle <rus31344@gmail.com>
2021-03-23 18:02:31 +0300

    починил сервер, была проблема с названием датабазы

--- lab1/task2/lab1/settings.py
+++ lab1/task2/lab1/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'lab1.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db_project_name'),
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
